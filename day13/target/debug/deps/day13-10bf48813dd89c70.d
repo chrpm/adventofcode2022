901854f134bc3d5946a8129f11ae5bee97a156c6
Christian McErlean <christian.mcerlean@gmail.com>
2022-12-25 20:32:29 -0700

    running

--- day13/target/debug/deps/day13-10bf48813dd89c70.d
+++
@@ -1,5 +0,0 @@
-/Users/christian/projects/advent_of_code_2022/day13/target/debug/deps/day13-10bf48813dd89c70.rmeta: src/main.rs
-
-/Users/christian/projects/advent_of_code_2022/day13/target/debug/deps/day13-10bf48813dd89c70.d: src/main.rs
-
-src/main.rs:
